9c027445ddfef2ee994fd7f197c0f2242d913c19
Willem Almstedt <walmstedt@gmail.com>
2017-09-19 12:56:59 +0200

    Updating gitignore

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,16 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
     entry: './src/index.jsx',
+    plugins: [
+        new CleanWebpackPlugin(['public']),
+        new HtmlWebpackPlugin({
+            title: 'TITLE',
+            filename: path.resolve(__dirname, 'public', 'index.html')
+        })
+    ],
     output: {
       filename: 'bundle.js',
       path: path.resolve(__dirname, 'public', 'js')
